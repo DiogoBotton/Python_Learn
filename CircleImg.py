f12c68a5d30e9b6c2fdad73a59b0446e899f5b33
DiogoBotton <diogo.luo@gmail.com>
2023-02-22 14:19:24 -0300

    desenhando circulo trigonometricamente com vetores

--- CircleImg.py
+++ CircleImg.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import math
 
 IMAGE_FOLDER = "img"
 
@@ -16,10 +17,13 @@ centroX, centroY = width // 2, height // 2
 raio_circulo = 64
 RED = (255,0,0)
 
-# Desenha um círculo no centro da imagem
+# Desenha um círculo no centro da imagem utilizando trigonometria
 for x in range(centroX-raio_circulo, centroX+raio_circulo):
     for y in range(centroY-raio_circulo, centroY+raio_circulo):
-        colorRGB = img.getpixel((x,y))
-        img.putpixel((x,y), RED)
+        # Calculo da magnitude do vetor
+        vetor_magnitude = math.sqrt((x-centroX)**2 + (y-centroY)**2)
+
+        if vetor_magnitude <= raio_circulo:
+            img.putpixel((x,y), RED)
 
 img.show()
